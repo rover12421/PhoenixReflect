85490dc78ac02469896f04b47de08de5438f3e85
Rover12421 <rover12421@163.com>
2018-05-27 13:19:33 +0800

    fix StaticObject

--- src/main/kotlin/com/rover12421/phoenix/reflect/component/entry/StaticObjectEntry.kt
+++ src/main/kotlin/com/rover12421/phoenix/reflect/component/entry/StaticObjectEntry.kt
@@ -3,9 +3,9 @@ package com.rover12421.phoenix.reflect.component.entry
 import java.lang.reflect.Field
 
 class StaticObjectEntry(field: Field): FieldEntry(field) {
-    fun setValue(value: Double) {
-        field.setDouble(null, value)
+    fun setValue(value: Any?) {
+        field.set(null, value)
     }
 
-    fun getValue() : Double = field.getDouble(null)
+    fun getValue() : Any? = field.get(null)
 }
